4c58971a9f3e67d9328e233e4ba7cb40fa020cbc
viswaah <vishwajenvish@gmail.com>
2022-10-13 17:19:58 +0530

    add canonical link

--- pages/[postpath].tsx
+++ pages/[postpath].tsx
@@ -66,9 +66,11 @@ const Post: React.FC<PostProps> = (props) => {
 		<>
 			<Head>
 				<meta property="og:title" content={post.title} />
+				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
+				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
@@ -89,7 +91,6 @@ const Post: React.FC<PostProps> = (props) => {
 					endpoint.replace(/(\/graphql\/)/, '') + post.featuredImage.node.uri
 				}
 				alt={post.featuredImage.node.altText || post.title}
-				width="300px"
 			/>
 			<p>{removeTags(post.excerpt)}</p>
 		</>
