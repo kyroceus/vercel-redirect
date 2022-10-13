5c79065b91d0a6d2d560c811ebb357040e6c5df1
viswaah <vishwajenvish@gmail.com>
2022-10-13 15:48:54 +0530

    add metadata for every link

--- pages/[postpath].tsx
+++ pages/[postpath].tsx
@@ -1,29 +1,70 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import Head from 'next/head';
 import { GetServerSideProps } from 'next';
+import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const graphQLClient = new GraphQLClient(endpoint);
+	const query = gql`
+		{
+			post(id: "/${ctx.query.postpath}/", idType: URI) {
+				id
+				excerpt
+				title
+				link
+				dateGmt
+				modifiedGmt
+				author {
+					node {
+						name
+					}
+				}
+				featuredImage {
+					node {
+						uri
+						altText
+					}
+				}
+			}
+		}
+	`;
+
+	const data = await graphQLClient.request(query);
+
 	const referringURL = ctx.req.headers?.referer || null;
 	return {
 		props: {
 			referringURL,
+			post: data.post,
+			fbclid: ctx.query.fbclid || null,
+			host: ctx.req.headers.host,
+			path: ctx.query.postpath,
 		},
 	};
 };
 
 interface PostProps {
 	referringURL?: string;
+	post: any;
+	host: string;
+	fbclid?: string | null;
+	path: string;
 }
 
 const Post: React.FC<PostProps> = (props) => {
-	const router = useRouter();
-	const { postpath } = router.query;
-
+	const { post, referringURL, host, path } = props;
 	return (
 		<>
-			<p>ReferringUrl: {props.referringURL || ''}</p>
-			<p>Post: {postpath}</p>
-			<p>fbclid: {router.query.fbclid}</p>
+			<Head>
+				<meta property="og:title" content={post.title} />
+				<meta property="og:description" content={post.excerpt} />
+				<meta property="og:url" content={`https://${host}/${path}`} />
+				<meta property="og:type" content="article" />
+				<meta property="og:site_name" content={host.split('.')[0]} />
+				<meta property="og:image" content={post.featuredImage.node.uri} />
+			</Head>
+			<h1>Post page</h1>
 		</>
 	);
 };
